92b6a3de5bb90401373c456263337800b67979a0
krigup789 <krigup789@gmail.com>
2026-06-29 15:30:14 +0530

    Remove AIChat component from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ import AIChat from "@/components/AIChat";
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <AIChat />
+      
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
